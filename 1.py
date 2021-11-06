3882948de8abb9e128a603dd589e11caf3273e35
Ahmed Shahriar <47810596+shahriar0247@users.noreply.github.com>
2021-11-05 20:40:16 -0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,24 +4,23 @@ from haystack.pipelines import ExtractiveQAPipeline
 from haystack.nodes import TfidfRetriever
 import os
 from haystack.utils import print_answers
-from haystack.nodes import TransformersReader
+from haystack.nodes import FARMReader, TransformersReader
 from haystack.document_stores import InMemoryDocumentStore
 
 
-document_store = InMemoryDocumentStore()
-
-
-dicts = [{'content': 'My name is Ahmed Shahriar. I am 19 years old. I like programming. I am a boy'}]
-document_store.write_documents(dicts)
-retriever = TfidfRetriever(document_store=document_store)
 
 reader = TransformersReader(model_name_or_path="distilbert-base-uncased-distilled-squad",
                             tokenizer="distilbert-base-uncased", use_gpu=-1)
 
+document_store = InMemoryDocumentStore()
+dicts = [{'content': 'My name is Conner Frost.My age is 20 years old. I like drinking. I am a boy'}, {'content': 'Robert Downy is Iron man'}]
+document_store.write_documents(dicts)
+retriever = TfidfRetriever(document_store=document_store)
 pipe = ExtractiveQAPipeline(reader, retriever)
+
+
 prediction = pipe.run(
-    query="What is my name", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
+    query="Who is Iron man", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
 )
+print(prediction['answers'][0].answer)
 
-
-print_answers(prediction, details="mininal")
