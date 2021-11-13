d43c1ae10c035e5016593c8e683b14d0aeb73606
Ahmed Shahriar <47810596+shahriar0247@users.noreply.github.com>
2021-11-14 05:20:09 +0600

    getting better answer
    now the problem is
    if i put "age" of 2 people
    if i ask the age of second guy
    it gives me the age of first guy
    as it first gives me multiple answers and i just print the first one
    yes in most the answers is correct
    so i gotta find the most common answer and print is out
    the problem is
    the most common answer has the question
    so now i am removing the question part and only using the "answer" to spit out the most common one
    which in theory should be the right one
    there is also a score with every answer, i will use that the next time 
    lol
    
    also this method didnt work idk why, it stil spits out 19, where should spit out 21

--- 1.py
+++ 1.py
@@ -7,20 +7,52 @@ from haystack.utils import print_answers
 from haystack.nodes import FARMReader, TransformersReader
 from haystack.document_stores import InMemoryDocumentStore
 
+reader =  TransformersReader(model_name_or_path="bert-large-uncased-whole-word-masking-finetuned-squad")
+pipe = None
 
 
-reader = TransformersReader(model_name_or_path="distilbert-base-uncased-distilled-squad",
-                            tokenizer="distilbert-base-uncased", use_gpu=-1)
+dicts = []
 
-document_store = InMemoryDocumentStore()
-dicts = [{'content': 'My name is Conner Frost.My age is 20 years old. I like drinking. I am a boy'}, {'content': 'Robert Downy is Iron man'}]
-document_store.write_documents(dicts)
-retriever = TfidfRetriever(document_store=document_store)
-pipe = ExtractiveQAPipeline(reader, retriever)
+def train(new_info):
+    document_store = InMemoryDocumentStore()
+    global dicts
+    dicts.append({'content': new_info})
+    document_store.write_documents(dicts)
+    retriever = TfidfRetriever(document_store=document_store)
+    global pipe
+    pipe = ExtractiveQAPipeline(reader, retriever)
+
+def ask(question):
+    global pipe
+    prediction = pipe.run(
+        query=question, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
+    )
+    return (find_answer(prediction))
+
+def find_answer(prediction):
+    answer_qualifications = {}
+    answers = []
+    for a in prediction['answers']:
+        answer = a.answer
+        for b in prediction['query'].split(" "):
+            answer = answer.replace(b,"") 
+        answers.append([answer, a.score])
+    for i, a in enumerate(answers):
+        answer_qualifications[i] = 0
+        for b in answers:
+            if (a[0] in b[0] or b[0] in a[0]) and a[0] != '':
+                answer_qualifications[i] += a[1]
+    highest_num = float(0)
+    index_of_highest = None
+    for a in answer_qualifications:
+        if answer_qualifications[a] > highest_num:
+            highest_num = answer_qualifications[a]
+            index_of_highest = a
+    return (prediction['answers'][index_of_highest].answer)
 
 
-prediction = pipe.run(
-    query="Who is Iron man", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
-)
-print(prediction['answers'][0].answer)
 
+
+train("Skyliner is a boy, he likes beetles. He is 19 years old")
+train("Conner is a boy, he likes angles, he is 21 years old")
+print(ask("how old is Conner"))
